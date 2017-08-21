e4644d552122f8ccfc632d28c56b227570c6e884
tayathorn <tayathorn@thinknet.co.th>
2017-08-21 10:39:17 +0700

    clear code

--- app/actions/index.js
+++ app/actions/index.js
@@ -3,8 +3,6 @@ import axios from 'react-native-axios'
 import {config} from '../config'
 
 export function fetchMessage() {
-  console.log('fetchMessage ja')
-  console.log('config.urlService :: ', config.urlService)
   return function(dispatch) {
     axios.get(config.urlService)
     .then((response) => {
